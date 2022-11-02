9be83d3b4a045253fe7ab2134f46db2eb11b5c9c
Daniel Carneiro <80073963+danrcarneiro@users.noreply.github.com>
2022-11-01 22:32:46 -0300

    Análises em Matlab

--- matlab/echomatlab.m
+++ matlab/echomatlab.m
@@ -1,7 +1,13 @@
-infile='entrada.wav';
-outfile='saida.wav';
-% read the sample waveform
-[x,Fs] = audioread(infile);
+clear
+infile='../assets/music.wav';
+outfile='../assets/saidaMatlabEchoFloat.wav';
+outfileFP = '../assets/saidaMatlabEchoFixed.wav';
+[x,Fs] = audioread(infile); %
+
+until = 44100*10;
+
+%% Double precision
+
 % normalize x to +/- 1 amplitude
 x = x ./ (max(abs(x)));
 % parameters 
@@ -23,5 +29,32 @@ figure(1)
 hold on
 plot(x,'r');
 plot(y,'b');
-title('Echoed and original Signal');
+title('Sinal original vs com echo');
+xlabel("Amostras")
+ylabel("Amplitude")
 %sound(y,Fs);
+
+%% Fixed point
+x_fixed = fi(x);
+x_fixedConv = double(x_fixed);
+y_fixed = zeros(length(double(x)),1);       % create empty out vector
+y_fixed(1:N_delay)=double(x(1:N_delay)); % to avoid referencing of negative samples
+% for each sample > N_delay
+for i = (N_delay+1):length(double(x_fixedConv))
+    y_fixed(i) = C*double(x_fixedConv(i)) + (1-C)*(double(x_fixedConv(i-N_delay)));   % add delayed sample
+    fprintf("%.2f%%\n", 100*i/length(x_fixedConv));
+end
+% write output
+% normalize y to +/- 1 amplitude
+y_fixed = y_fixed ./ (max(abs(y_fixed)));
+y_fixedObj = fi(y_fixed);
+y_fixedConverted = double(y_fixedObj);
+audiowrite(outfileFP, double(y_fixedObj), Fs);
+figure(2)
+hold on
+plot(y,'r');
+plot(y_fixedConverted,'b');
+title('Sinal original e ponto fixo - Fixed point');
+%sound(y,Fs);
+
+err = immse(y_fixedConverted,y)
--- matlab/flangermatlab.m
+++ matlab/flangermatlab.m
@@ -1,7 +1,8 @@
 clear
 close all
-infile='entrada.wav';
-outfile='saida.wav';
+infile='../assets/music.wav';
+outfile='../assets/saidaMatlabFlangerFloat.wav';
+outfileFP = '../assets/saidaMatlabFlangerFixed.wav';
 [x,Fs] = audioread(infile);
 x = x(1:2000000);
 fs=Fs;
@@ -20,7 +21,28 @@ figure(1)
 hold on
 plot(y,'r');
 plot(x,'b');
-title('Flanger and original Signal');
+title('Sinal original vs com flanger');
+xlabel("Amostras")
+ylabel("Amplitude")
+
+
+%% Fixed point
+
+x_fixed = fi(x);
+x_converted = double(x);
+[y_fixed, LFO] = flangerFixed(x,vmix,vdelay,vwidth,vrate,Fs);
+
+figure(2)
+hold on
+plot(y,'r');
+plot(y_fixed,'b');
+title('Sinal em double vs ponto fixo');
+%sound(y,Fs);
+
+err = immse(y_fixed,y)
+
+
+%% Function
 
 
 function [out] = flanger(in,mix,delay,width,rate,fs)
@@ -54,5 +76,39 @@ for i=maxDelaySamp+1:length(in) % starting from next sample
 end
 
 out=downsample(out,4);
+
+end
+
+function [out,LFO] = flangerFixed(in,mix,delay,width,rate,fs)
+%FLANGER simulates a guitar flanger effect pedal
+%   IN - input vector
+%   MIX - depth - amt of delayed signal added to IN (0 to 1)
+%   DELAY - min delay time - 100usec to 10msec (in msec) 0.1 to 10
+%   WIDTH - sweep depth - how wide sweep is (100nsec to 10msec) 
+%           (in msec, 0.0001)
+%   RATE - frequency of LFO - 0.05 to 5 Hz
+
+in=interp1(1:length(in),in,1:.25:length(in));
+fsn=fs*4;
+
+minDelaySamp=ceil(delay*fsn/1000); %convert to msec, then samples
+maxDelaySamp=ceil((delay+width)*fsn/1000); %convert to msec, then samples
+n=(1:length(in)+maxDelaySamp)'; %how long to extend in by for LFO
+LFO=double(fi(sawtooth(2*pi*rate/(fsn)*n,.5))); %sawtooth more commonly used in flangers
+delayTimeSamples=(delay+width/2+width/2*LFO)*fsn/1000; 
+% instantaneous delay in samples (computed by looking at graph from class
+% PDF)
+
+out=zeros(length(in)+minDelaySamp,1); %initialized output vec
+
+out(1:maxDelaySamp)=in(1:maxDelaySamp); 
+% copy front of signal before min delay
+
+for i=maxDelaySamp+1:length(in) % starting from next sample
+    delaySamples=ceil(delayTimeSamples(i)); %whole number of current delay
+    out(i)=in(i)+mix*out(i-delaySamples); %add input and fraction of delay 
+end
+
+out=double(fi(downsample(out,4)));
 
 end
